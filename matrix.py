595550906793e868b58b033640fab63fa3c17e3b
Xian Jian Zhang <xianjianzhang.yahoo@gmail.com>
2020-03-31 19:26:47 -0500

    Adding matrix class with addition functionality

---
+++ matrix.py
@@ -0,0 +1,36 @@
+class Matrix():
+  def __init__(self, row, col, input_num):
+    self.col = col
+    self.row = row
+    self.input_num = input_num
+    self.matrix = Matrix.create_matrix(self)
+
+  def __repr__(self):
+    return Matrix.getMatrix()
+
+  #Creates the actual matrix
+  def create_matrix(self):
+    empty_list = []
+    for x in range(self.row):
+      row_list = []
+      for y in range(self.col):
+        index = ((x+1)*(y+1))-1
+        row_list.append(self.input_num[index])
+      empty_list.append(row_list)
+    return (empty_list)
+
+  def getMatrix(self):
+    return(self.matrix)
+
+  def addMatrix(Matrix_object, Matrix_object2):
+    matrix1 = Matrix_object.getMatrix()
+    matrix2 = Matrix_object2.getMatrix()
+    new_matrix=[]
+    for x in range(len(matrix1)):
+      row_matrix = []
+      for y in range(len(matrix1[x])):
+        point = matrix1[x][y]+ matrix2[x][y]
+        row_matrix.append(point)
+      new_matrix.append(row_matrix)
+    return new_matrix
+
